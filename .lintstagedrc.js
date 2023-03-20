971ce163fd99944d5943cc6f94c64f7decafa1a1
Chathan Driehuys <chathan@driehuys.com>
2023-03-20 15:51:28 +0000

    feat: navbar

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,7 @@
 const path = require("path");
 
+// Required in order for `next` to accept multiple file names.
+// https://nextjs.org/docs/basic-features/eslint#lint-staged
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,20 @@
+import Link from "next/link";
 import "./global.css";
 
 export const metadata = {
   title: "My App",
   description: "A playground for Next.js",
 };
+
+const Header = () => (
+  <header className="mb-4 bg-blue-500 p-2 shadow-md">
+    <div className="mx-auto max-w-4xl">
+      <Link href="/">
+        <h1 className="text-xl text-white">My App</h1>
+      </Link>
+    </div>
+  </header>
+);
 
 export default function RootLayout({
   children,
@@ -12,7 +23,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header />
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
