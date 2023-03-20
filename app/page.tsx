dbe6048a2965a8c7127a43c18b68fe08bce8d75a
Chathan Driehuys <chathan@driehuys.com>
2023-03-20 15:57:24 +0000

    fix: deprecated `Head` tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,14 @@
-import Head from "next/head";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>My App</title>
-      </Head>
-      <main>
-        <h1 className="text-3xl">My App</h1>
-
-        <h2 className="text-xl">Pages</h2>
-        <ul>
-          <li>
-            <Link href="/log-in">Log In</Link>
-          </li>
-        </ul>
-      </main>
+      <h2 className="text-xl">Pages</h2>
+      <ul>
+        <li>
+          <Link href="/log-in">Log In</Link>
+        </li>
+      </ul>
     </>
   );
 }
